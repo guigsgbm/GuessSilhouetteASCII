0bec498bf4a4fe6fee9f3934e8c26808d215a3fd
unknown <guigsgbm@gmail.com>
2024-01-15 08:45:49 -0300

    first one

---
+++ Src/Domain/Character.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Text.Json.Nodes;
+using System.Threading.Tasks;
+
+namespace Domain;
+
+public class Character
+{
+    public int Id { get; }
+
+    public string Name 
+    {
+        get { return _name; }
+        private set {  _name = value; } 
+    }
+    private string _name;
+
+    public string Description 
+    {
+        get { return _description; }
+        set { _description = value; } 
+    }
+    private string _description;
+
+    public enumCategory Category
+    {
+        get { return _category; }
+        private set { _category = value; }
+    }
+    private enumCategory _category;
+    public enum enumCategory
+    {
+        Dragon_Ball,
+        Naruto,
+        One_Piece
+    };
+
+    public Character(string name, string description, enumCategory category)
+    {
+        Name = name;
+        Description = description;
+        Category = category;
+    }
+
+}
---
+++ Src/Out/Program.cs
@@ -0,0 +1,8 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Domain;
+
+Console.WriteLine("Hello, World!");
+
+var char1 = new Character("Naruto", "Naturo Classico", Character.enumCategory.Dragon_Ball);
+
+Console.WriteLine(char1.Category);
